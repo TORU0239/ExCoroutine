30147e94ffbc6569d4da110c84c065dfab9d9d3d
Toru Choi <blueocean239@gmail.com>
2019-09-17 16:37:55 +0800

    Initial commit

---
+++ app/src/main/java/sg/toru/excoroutine/MainActivity.kt
@@ -0,0 +1,12 @@
+package sg.toru.excoroutine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
